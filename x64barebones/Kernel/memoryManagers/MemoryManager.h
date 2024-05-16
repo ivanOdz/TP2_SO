46dcc3f24074fc6a580eb0708ed3c697328b164d
ivanOdz <miodzomek@gmail.com>
2024-05-16 16:57:49 -0300

    Memmory Manager by us

---
+++ x64barebones/Kernel/memoryManagers/MemoryManager.h
@@ -0,0 +1,13 @@
+#ifndef MEMORY_MANAGER_H
+#define MEMORY_MANAGER_H
+
+#include <libc.h>
+
+typedef struct MemoryManagerCDT *MemoryManagerADT;
+
+/*MemoryManagerADT createMemoryManager(void *const restrict managedMemory, uint64_t memAmount);
+void *allocMemory(const uint64_t size);
+void free(void *ptrAllocatedMemory);
+MemoryInfo *getMemoryInfo();*/
+
+#endif
--- x64barebones/Kernel/memoryManagers/buddyMemoryManager.c
+++ x64barebones/Kernel/memoryManagers/buddyMemoryManager.c
@@ -4,17 +4,3 @@ typedef struct MemoryManagerCDT {
 	char *nextAddress;
 } MemoryManagerCDT;
 
-MemoryManagerADT createMemoryManager(void *const restrict memoryForMemoryManager, void *const restrict managedMemory) {
-	MemoryManagerADT memoryManager = (MemoryManagerADT) memoryForMemoryManager;
-	memoryManager->nextAddress = managedMemory;
-
-	return memoryManager;
-}
-
-void *allocMemory(MemoryManagerADT const restrict memoryManager, const size_t memoryToAllocate) {
-	char *allocation = memoryManager->nextAddress;
-
-	memoryManager->nextAddress += memoryToAllocate;
-
-	return (void *) allocation;
-}
